4e990a41342e1d440a75c5e9c410cc38f64d32cd
unknown <saadallahibraim17@gmail.com>
2024-01-15 07:25:07 +0100

    adding the skills

--- JS/jsfile.js
+++ JS/jsfile.js
@@ -1,2 +1,24 @@
 // const navLinks = document.querySelectorAll('.navbar');
 // const windowPathname = window.location.pathname;
+
+const progrossBars = document.querySelectorAll('.progress'); //create the intersection observer
+const observer = new IntersectionObserver((enteries) => {
+    // loop through the entries
+    enteries.forEach((entry) => {
+        // check if the entry is intersecting
+        if(entry.isIntersecting){
+            // add the active class to the progress bars 
+            progressBars.forEach((progress) => {
+                progress.classaList.add("active");
+            });
+        }
+        else{
+            // remove the active class from the progess bars
+            progrossBars.forEach((progress) => {
+                progress.classList.remove("active");
+            });
+        }
+    })
+});
+// observe te progress bars
+observer.observe(document.querySelector(".progress"));
