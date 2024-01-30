db4e21f7689cae892ef549a0c9c935e561ddf176
unknown <saadallahibraim17@gmail.com>
2024-01-30 12:41:13 +0100

    adding the active page

--- JS/jsfile.js
+++ JS/jsfile.js
@@ -1,24 +1,22 @@
-// const navLinks = document.querySelectorAll('.navbar');
-// const windowPathname = window.location.pathname;
 
-const progressBars = document.querySelectorAll('.progress'); //create the intersection observer
-const observer = new IntersectionObserver((entries) => {
-    // loop through the entries
-    entries.forEach((entry) => {
-        // check if the entry is intersecting
-        if(entry.isIntersecting){
-            // add the active class to the progress bars 
-            progressBars.forEach((progress) => {
-                progress.classaList.add("active");
-            });
-        }
-        else{
-            // remove the active class from the progess bars
-            progressBars.forEach((progress) => {
-                progress.classList.remove("active");
-            });
-        }
-    })
+
+
+$(document).on('click', 'ul li', function(){
+    $(this).addClass('active').siblings().removeClass('active')
 });
-// observe te progress bars
-observer.observe(document.querySelector(".progress"));
+
+
+// var links = document.querySelectorAll('.navbar a');
+
+//         // Loop through each anchor tag
+//         links.forEach(function(link) {
+//             // Add click event listener to each link
+//             link.addEventListener('click', function(event) {
+//                 // Remove 'active' class from all links
+//                 links.forEach(function(item) {
+//                     item.classList.remove('active');
+//                 });
+//                 // Add 'active' class to the clicked link
+//                 this.classList.add('active');
+//             });
+//         });
