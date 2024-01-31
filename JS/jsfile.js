0d71cd1bb353b8f8a24f7632ab759362c2f965bb
unknown <saadallahibraim17@gmail.com>
2024-01-31 08:47:00 +0100

    done with the acive navbars

--- JS/jsfile.js
+++ JS/jsfile.js
@@ -1,22 +1,11 @@
 
 
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll(".navbar ul a");
+const menuLength = menuItem.length;
 
-$(document).on('click', 'ul li', function(){
-    $(this).addClass('active').siblings().removeClass('active')
-});
-
-
-// var links = document.querySelectorAll('.navbar a');
-
-//         // Loop through each anchor tag
-//         links.forEach(function(link) {
-//             // Add click event listener to each link
-//             link.addEventListener('click', function(event) {
-//                 // Remove 'active' class from all links
-//                 links.forEach(function(item) {
-//                     item.classList.remove('active');
-//                 });
-//                 // Add 'active' class to the clicked link
-//                 this.classList.add('active');
-//             });
-//         });
+for(var i = 0; i < menuLength; i++) {
+    if(menuItem[i].href === currentLocation){
+        menuItem[i].className = "active";
+    }
+}
