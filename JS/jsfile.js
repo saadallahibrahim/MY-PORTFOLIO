562134d1830f259033143cb3a6db5810e8e51cec
unknown <saadallahibraim17@gmail.com>
2024-02-03 16:57:01 +0100

    find the solution to it

--- JS/jsfile.js
+++ JS/jsfile.js
@@ -9,3 +9,26 @@ for(var i = 0; i < menuLength; i++) {
         menuItem[i].className = "active";
     }
 }
+
+
+
+function animateProgressBar(barId, percentage) {
+    var bar = document.getElementById(barId);
+    var width = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= percentage) {
+            clearInterval(id);
+        } else {
+            width++;
+            bar.style.width = width + "%";
+            bar.innerHTML = width + "%";
+        }
+    }
+}
+
+animateProgressBar("htmlBar", 90);
+animateProgressBar("cssBar", 85);
+animateProgressBar("jsBar", 80);
+animateProgressBar("phpBar", 75);
+animateProgressBar("mysqlBar", 70);
